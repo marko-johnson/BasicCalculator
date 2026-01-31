550d4212432a91be3f1161ee866f4e6c8f0d2364
mjohnson <mr.markoeddie@gmail.com>
2026-01-31 09:50:39 -0500

    added class Arithmetic

---
+++ Arithmetic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    public class Arithmetic
+    {
+        public static int add(int num1, int num2) => num1 + num2;
+
+        public static int subtract(int num1, int num2) => num1 - num2;
+
+        public static int multiply(int num1, int num2) => num1 * num2;
+
+        public static int divide(int num1, int num2) => num1 / num2;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,37 +22,25 @@ int num1 = int.Parse(Console.ReadLine());
 Console.Write("Please enter 2nd number: ");
 int num2 = int.Parse(Console.ReadLine());
 
-int add(int num1, int num2) => num1 + num2;
-
-int subtract(int num1, int num2) => num1 - num2;
-
-int multiply(int num1, int num2) => num1 * num2;
-
-int divide(int num1, int num2) => num1 / num2;
-
-
 Console.WriteLine("Please type option, 'add', 'subtract', 'multiply', or 'divide': ");
 
 string menuSelection = Console.ReadLine();
 
-
-
-
 switch (menuSelection)
 {
     case "add":
-        Console.WriteLine($"Addition: {num1} + {num2} = {add(num1, num2)}");
+        Console.WriteLine($"Addition: {num1} + {num2} = {BasicCalculator.Arithmetic.add(num1, num2)}");
         break;
     case "subtract":
-        Console.WriteLine($"Subtraction: {num1} - {num2} = {subtract(num1, num2)}");
+        Console.WriteLine($"Subtraction: {num1} - {num2} = {BasicCalculator.Arithmetic.subtract(num1, num2)}");
         break;
     case "multiply":
-        Console.WriteLine($"Multiplication: {num1} * {num2} = {multiply(num1, num2)}");
+        Console.WriteLine($"Multiplication: {num1} * {num2} = {BasicCalculator.Arithmetic.multiply(num1, num2)}");
         break;
     case "divide":
         if (num1 % num2 == 0)
         {
-            Console.WriteLine($"Division: {num1} / {num2} = {divide(num1, num2)}");
+            Console.WriteLine($"Division: {num1} / {num2} = {BasicCalculator.Arithmetic.divide(num1, num2)}");
         }
         else
             Console.WriteLine("Next time enter a numbers that divides evenly without remainders");
@@ -61,4 +49,17 @@ switch (menuSelection)
         Console.WriteLine("Unknown");
         break;
 }
+
+
+   
+
+
+
+
+
+
+
+
+
+
 

019765962cbbdfdb55feff26c865ad20385fc4f3
mjohnson <mr.markoeddie@gmail.com>
2026-01-31 09:26:16 -0500

    added arithmetic methods

--- Program.cs
+++ Program.cs
@@ -22,7 +22,25 @@ int num1 = int.Parse(Console.ReadLine());
 Console.Write("Please enter 2nd number: ");
 int num2 = int.Parse(Console.ReadLine());
 
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
 
+int subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int multiply(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int divide(int num1, int num2)
+{
+    return num1 / num2;
+}
 
 Console.WriteLine("Please type option, 'add', 'subtract', 'multiply', or 'divide': ");
 
@@ -33,19 +51,19 @@ string menuSelection = Console.ReadLine();
 
 switch (menuSelection)
 {
-    case "add": 
-        Console.WriteLine($"Addition: {num1} + {num2} = {num1 + num2}");
+    case "add":
+        Console.WriteLine($"Addition: {num1} + {num2} = {add(num1, num2)}");
         break;
     case "subtract":
-        Console.WriteLine($"Subtraction: {num1} - {num2} = {num1 - num2}");
+        Console.WriteLine($"Subtraction: {num1} - {num2} = {subtract(num1, num2)}");
         break;
     case "multiply":
-        Console.WriteLine($"Multiplication: {num1} * {num2} = {num1 * num2}");
+        Console.WriteLine($"Multiplication: {num1} * {num2} = {multiply(num1, num2)}");
         break;
     case "divide":
         if (num1 % num2 == 0)
         {
-            Console.WriteLine($"Division: {num1} / {num2} = {num1 / num2}");
+            Console.WriteLine($"Division: {num1} / {num2} = {divide(num1, num2)}");
         }
         else
             Console.WriteLine("Next time enter a numbers that divides evenly without remainders");
